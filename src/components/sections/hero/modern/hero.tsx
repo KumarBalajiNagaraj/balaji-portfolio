4a5fc40c30b8b31af1928da78792aeb0b8e158ef
KumarBalajiNagaraj <balaji.volvocars@gmail.com>
2025-01-04 03:42:01 +0100

    feat: add touch interaction to Hero component for visibility animation

--- src/components/sections/hero/modern/hero.tsx
+++ src/components/sections/hero/modern/hero.tsx
@@ -1,10 +1,10 @@
 'use client';
 // Credit: Cuberto
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import { useRef } from 'react';
 import TextReveal from '@/components/motion/text-reveal';
 import Reveal from '@/components/reveal';
@@ -12,6 +12,20 @@ import ParallaxImage from '@/components/motion/parallax-image';
 
 function Hero() {
   const container = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(false);
+  const controls = useAnimation();
+
+
+
+  const handleTouch = () => {
+    setVisible(true);
+    controls.start({
+      opacity: [0, 1, 0],
+      transition: { duration: 1.5, ease: 'easeInOut' },
+    });
+
+    setTimeout(() => setVisible(false), 1500); // Reset after 1.5 seconds
+  };
 
   return (
     <section
@@ -22,9 +36,13 @@ function Hero() {
       <div className="relative z-10 h-[42.5dvh] md:h-[51.2dvh] md:min-h-[50dvh] xl:h-[61.2dvh] flex flex-col justify-center items-center">
         <div className="flex w-full flex-col items-center justify-center px-4 md:px-6 text-center">
           <Reveal>
-            <p className="text-sm font-semibold uppercase tracking-wide text-muted-foreground">
-              Human | Product | Designer | Developer | Innovator | Leader <br />
+            <p
+              className="text-sm font-semibold uppercase tracking-wide sm:text-xs md:text-sm lg:text-base"
+            >
+              Human | Designer - Developer | Innovator
             </p>
+
+
           </Reveal>
           <h1 className="text-4xl font-bold leading-tight tracking-tight sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
             <TextReveal>Hi, I'm <br /> Balaji Nagaraj Kumar</TextReveal>
